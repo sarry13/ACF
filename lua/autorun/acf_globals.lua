15e378b98d1a453eb9f0f0dbe795b2266d30489e
Fervidusletum <fervidusletum@gmail.com>
2019-09-05 05:55:21 -0700

    ghosteh particles

--- lua/autorun/acf_globals.lua
+++ lua/autorun/acf_globals.lua
@@ -2,7 +2,7 @@ ACF = {}
 ACF.AmmoTypes = {}
 ACF.MenuFunc = {}
 ACF.AmmoBlacklist = {}
-ACF.Version = 657 -- REMEMBER TO CHANGE THIS FOR GODS SAKE, OMFG!!!!!!! -wrex   Update the changelog too! -Ferv
+ACF.Version = 659 -- REMEMBER TO CHANGE THIS FOR GODS SAKE, OMFG!!!!!!! -wrex   Update the changelog too! -Ferv
 ACF.CurrentVersion = 0 -- just defining a variable, do not change
 
 ACF.Year = 1945
